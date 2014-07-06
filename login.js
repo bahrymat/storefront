cf426cb3d2d441d8f8966f3711934502e6f9d653
bahrymat <matthew.bahry@yahoo.ca>
2014-07-06 18:40:59 -0400

    redirect back to index after log

--- login.js
+++ login.js
@@ -1,4 +1,7 @@
- function setLoginCookie(email) {
+function setLoginCookie(email) {
 	document.cookie="email=" + email;
 	console.log(document.cookie);
+	setTimeout(function(){
+		window.location.replace("index.html");
+	}, 3000);
 }
