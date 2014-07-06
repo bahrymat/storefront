aa67359e17ba3122c550ce4eef9d19dc3969c9d1
bahrymat <matthew.bahry@yahoo.ca>
2014-07-06 18:23:13 -0400

    logins work

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function hideLogin() {
 		$('#login_button').fadeIn(300);
 	});
 }
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+	}
+	return "";
+}
+
+if (getCookie("email")) {
+	$(".loggedin").removeClass("hidden");
+} else {
+	$(".loggedout").removeClass("hidden");
+}
