aa1155cfebb1b440110990fd57a5c5ac227accc9
lawrence-j <j4sonlawrence@gmail.com>
2014-08-06 23:59:01 -0400

    report mostly filled out

---
+++ report.js
@@ -0,0 +1,476 @@
+
+var fppreData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [28,29,32,37,45,49,66,68,13989]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [27,29,29,32,48,52,54,55,251]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [26,27,28,28,35,36,38,38,259]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [10,11,12,13,16,19,35,36,37]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [1,1,1,1,2,2,4,6,30]
+		}
+
+
+	]
+}
+    var fppre = document.getElementById('fppre').getContext('2d');
+    new Chart(fppre).Line(fppreData, {datasetFill :false});
+ var fppostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [58,65,70,78,147,151,162,164,287]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [35,38,41,44,59,61,62,62,261]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [31,33,35,35,54,62,62,63,253]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [10,10,10,11,12,33,35,45,47]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [1,1,1,1,2,2,2,3,30]
+		}
+
+
+	]
+}
+    var fppost = document.getElementById('fppost').getContext('2d');
+    new Chart(fppost).Line(fppostData, {datasetFill :false});
+var abpreData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [28,29,29,30,44,45,46,28,19913]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [26,27,34,36,66,77,81,81,244]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [26,28,35,37,40,78,81,82,245]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [8,9,9,9,11,15,36,37,38]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [1,1,1,1,1,1,2,2,26]
+		}
+
+
+	]
+}
+    var abpre = document.getElementById('abpre').getContext('2d');
+    new Chart(abpre).Line(abpreData, {datasetFill :false});
+
+var abpostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [29,33,39,44,71,74,75,76,246]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [28,38,52,65,87,89,101,103,241]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [36,37,43,44,47,56,67,67,246]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [9,9,9,9,10,24,35,35,36]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [1,1,1,1,1,2,2,3,23]
+		}
+
+
+	]
+}
+    var abpost = document.getElementById('abpost').getContext('2d');
+    new Chart(abpost).Line(abpostData, {datasetFill :false});
+var expostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [839,871,914,944,986,1912,1985,2021,2194]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [352,444,487,511,548,1454,1555,1614,1632]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [167,194,209,217,222,231,238,241,244]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [39,46,49,50,62,63,64,71,77]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [3,3,4,4,5,5,14,15,28]
+		}
+
+
+	]
+}
+    var expost = document.getElementById('expost').getContext('2d');
+    new Chart(expost).Line(expostData, {datasetFill :false});
+var conpostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [299,376,543,588,1468,1598,1616,1635,1950]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [284,308,317,325,363,390,1298,1316,1330]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [119,135,143,149,154,156,158,163,169]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [30,35,36,37,39,48,55,56,60]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [3,3,3,4,4,4,7,14,17]
+		}
+
+
+	]
+}
+    var conpost = document.getElementById('conpost').getContext('2d');
+    new Chart(conpost).Line(conpostData, {datasetFill :false});
+var mspostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [785,932,974,1014,1804,1931,2220,2242,2262]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [580,622,641,644,657,1541,1624,1825,1849]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [224,242,247,253,264,269,282,282,293]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [51,58,63,65,72,81,86,88,93]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [5,6,7,8,8,9,15,17,26]
+		}
+
+
+	]
+}
+    var mspost = document.getElementById('mspost').getContext('2d');
+    new Chart(mspost).Line(mspostData, {datasetFill :false});
+var sfppostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [429,465,510,552,1519,1615,1816,1825,1830]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [320,365,385,395,418,437,483,503,1397]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [61,69,70,70,80,82,86,93,99]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [30,35,36,36,42,47,52,55,58]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [3,3,3,3,4,4,7,13,17]
+		}
+
+
+	]
+}
+    var sfppost = document.getElementById('sfppost').getContext('2d');
+    new Chart(sfppost).Line(sfppostData, {datasetFill :false});
+
+var spppostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [330,368,437,538,1438,1530,1608,1805,1812]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [311,355,379,393,422,438,507,1364,1389]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [123,135,149,154,159,170,171,174,179]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [30,35,36,36,38,47,54,57,61]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [3,3,4,4,4,5,7,14,21]
+		}
+
+
+	]
+}
+    var spppost = document.getElementById('spppost').getContext('2d');
+    new Chart(spppost).Line(spppostData, {datasetFill :false});
+var scppostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [958,1126,1192,1227,2173,2321,3478,3490,3498]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [559,566,569,570,572,602,1586,1605,1622]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [218,234,242,243,258,262,272,273,290]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [52,60,63,66,74,82,90,96,123]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [5,6,7,8,8,9,16,17,41]
+		}
+
+
+	]
+}
+    var scppost = document.getElementById('scppost').getContext('2d');
+    new Chart(scppost).Line(scppostData, {datasetFill :false});
+var ssppostData = {
+	labels : ["50","66","75","80","90","95", "98", "99", "100"],
+	datasets : [
+{
+			strokeColor : "#000",
+			pointColor : "#000",
+			pointStrokeColor : "#000",
+			data : [947,1024,1055,1073,2086,3442,3476,3482,3488]
+		},
+
+{
+			strokeColor : "#00f",
+			pointColor : "#00f",
+			pointStrokeColor : "#000",
+			data : [579,609,621,623,632,649,1626,1645,1666]
+		},
+
+
+{
+			strokeColor : "#0f0",
+			pointColor : "#0f0",
+			pointStrokeColor : "#000",
+			data : [228,249,263,266,276,285,306,314,318]
+		},
+
+{
+			strokeColor : "#f00",
+			pointColor : "#f00",
+			pointStrokeColor : "#000",
+			data : [52,60,64,68,75,82,85,89,94]
+		},
+		{
+			strokeColor : "#999",
+			pointColor : "#fff",
+			pointStrokeColor : "#000",
+			data : [5,7,8,8,8,9,14,18,28]
+		}
+
+
+	]
+}
+    var ssppost = document.getElementById('ssppost').getContext('2d');
+    new Chart(ssppost).Line(ssppostData, {datasetFill :false});
